38e0619e7e65e0a86ffeb0247d4570acdf03e6ae
rebootless <sbox.simple@protonmail.com>
2026-07-28 01:43:21 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ set -euo pipefail
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 if [[ -d build ]]; then
-    echo "[*] Removing old build directory..."
+    echo "Removing old build directory..."
     rm -rf build
 fi
 
-echo "[*] Configuring..."
+echo "Configuring..."
 cmake -B build -DCMAKE_BUILD_TYPE=Release -DUSE_PANDOC=OFF
 
-echo "[*] Building..."
+echo "Building..."
 cmake --build build -j"$(nproc)"
 
-echo "[+] Done."
+echo "Done."
